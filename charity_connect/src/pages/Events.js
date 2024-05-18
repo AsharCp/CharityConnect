f9928cf02f9d2f29edf64791b693226e04da2b6b
Your Name <you@example.com>
2024-05-18 10:04:18 +0530

    Event Page

--- charity_connect/src/pages/Events.js
+++ charity_connect/src/pages/Events.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import EventData from '../Events.json'
 
 const Events = () => {
+  useEffect(() => {
+    console.log(EventData)
+  },)
+  
   return (
-    <div>Events</div>
+    <div className='m-10 flex flex-row flex-wrap justify-center'>
+      {EventData.Events.map(data=>(
+        <div key={data.id} className='w-2/12 flex flex-col items-center text-center border-2 border-gray-500 rounded m-3'>
+          <img src={data.url} alt='Image' />  
+          <p className='p-2  font-semibold'>{data.name}</p>
+        </div>
+
+      ))
+
+      }
+      
+    </div>
   )
 }
 
